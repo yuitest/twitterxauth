436b3038b28dda78c8c1b95909021a0d3a813f6e
yuitest <yuitest+git@cjhat.net>
2014-09-25 22:25:03 +0900

    first commit.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+version = '0.0.1'
+
+setup(
+    name='twitterxauth',
+    version=version,
+    description="thin wrapper module for xAuth twitter.com",
+    long_description="""\
+""",
+    classifiers=[
+    ],
+    keywords='twitter xauth',
+    author='yuitest',
+    author_email='yuitest+git@cjhat.net',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+""",
+)
